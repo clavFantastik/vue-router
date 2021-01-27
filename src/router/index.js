bcfe216e59690482dd8e9bb8dab1d06e1dc5fd2e
Artyom Fomenko <fantastik.play.2000@gmail.com>
2021-01-27 17:32:15 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contacts from '../views/Contacts.vue'
+import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +18,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About,
   },
   {
     path: '/contacts',
@@ -24,7 +26,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (contacts.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "contacts" */ '../views/Contacts.vue')
+    component: Contacts,
+
   }
 
 ]
